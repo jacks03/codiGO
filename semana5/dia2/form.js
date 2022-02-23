c6ae2cfe99f1d07992bc6f36d54c10f9731ff016
jacks03 <kjarisalfaro14@gmail.com>
2022-02-23 00:14:15 -0500

    avance con los header

---
+++ semana5/dia2/form.js
@@ -0,0 +1,57 @@
+let nombres = [];
+
+function obtenerValoresDelInput(event) {
+  //* event contiene muchos atributos del input
+  //* los atributos mas importante que vamos a ver hoy el value y el name
+  //? value = sera el contenido del input
+  //? name = sera el nombre que le asigne a mi input
+
+  //TODO: event es un objeto y la propiedada que contiene al value y al name se
+  //TODO: llama "target"
+  //! accediendo al value
+  console.log(event.target.value);
+  console.log(event.target.name);
+  // si fuera un objeto cual seria el key?
+  nombres.push({
+    [event.target.name]: event.target.value,
+  });
+}
+
+// vamos a crear una table de persona cual sea como un registe, es decir tengamos input y con boton
+// se agregue a mi tabla
+
+//* vamos a validar el correo
+// regex es un conjunto de caracteres el cual me dice si un elementos es valido
+function validarCorreo(event) {
+  //* sepan que este regex me va a servir para validar que el correo tenga un formato correcto
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  //* regex tiene la propiedad test me permite validar si es correcto y esto retorna true y false
+  const emailError = document.querySelector("#email-error");
+
+  //! Si el regex es false entonces hazle un display block al span si no el display sera none
+  if (!emailRegex.test(event.target.value)) {
+    emailError.style.display = "block";
+  } else {
+    emailError.style.display = "none";
+  }
+}
+
+function validarPassword(event) {
+    const password = event.target.value.length
+    const passwordError = document.querySelector("#password-error")
+    if(password <= 8) {
+        passwordError.style.display = "block"
+    } else {
+        passwordError.style.display ="none"
+    }
+}
+
+function validarNumber(event) {
+    const number = event.target.value.length
+    const numberError = document.querySelector("#number-error")
+    if(number <= 9) {
+        numberError.style.display = "block"
+    } else {
+        numberError.style.display ="none"
+    }
+}
